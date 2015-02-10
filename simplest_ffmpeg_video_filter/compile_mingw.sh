35ba5fe9d621e85c510738b5dd3be42a7e0f6448
unknown <leixiaohua1020@126.com>
2015-02-10 20:41:30 +0800

    fix

--- simplest_ffmpeg_video_filter/compile_mingw.sh
+++ simplest_ffmpeg_video_filter/compile_mingw.sh
@@ -11,4 +11,4 @@
 #compile
 g++ simplest_ffmpeg_video_filter.cpp -g -o simplest_ffmpeg_video_filter.exe \
 -I /usr/local/include -L /usr/local/lib \
--lmingw32 -lSDLmain -lSDL -lavformat -lavcodec -lavutil -lswscale
+-lmingw32 -lSDLmain -lSDL -lavformat -lavcodec -lavutil -lavfilter -lswscale
